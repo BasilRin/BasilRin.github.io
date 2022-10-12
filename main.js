0194738845ff7fa5aec224cdcddfe6f7d3e19b10
BasilRin <basil.aiyub@hotmail.com>
2022-10-11 17:28:37 -0700

    image sizing
    add javascript array for image sizing

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 // Get the elements with class="column"
 let elements = document.getElementsByClassName("column");
+const sizeArray = [
+  "25%",
+  "20%",
+  "15%",
+  "10%",
+  "5%",
+  "25%",
+  "20%",
+  "15%",
+  "10%",
+  "5%",
+];
 
-fourImages = (elements) => {
+let fourImages = () => {
   for (let i = 0; i < elements.length; i++) {
-    console.log(i);
+    let randomNumber = Math.floor(Math.random() * 10);
+    elements[i].style.msFlex = sizeArray[randomNumber];
+    elements[i].style.flex = sizeArray[randomNumber];
+    console.log(randomNumber);
   }
 };
 
-fourImages(elements);
+fourImages();
