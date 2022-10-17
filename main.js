50e455a4532e63acebc539285f8b23a544e070e3
BasilRin <basil.aiyub@hotmail.com>
2022-10-16 19:48:01 -0700

    new images
    more images

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 // Get the elements with class="column"
 let elements = document.getElementsByClassName("column");
 const sizeArray = [
@@ -19,38 +18,36 @@ let fourImages = () => {
     let randomNumber = Math.floor(Math.random() * 10);
     elements[i].style.msFlex = sizeArray[randomNumber];
     elements[i].style.flex = sizeArray[randomNumber];
-    console.log(randomNumber);
   }
 };
 
 fourImages();
 
 function drawPattern(img, size) {
-  var canvas = document.getElementById('canvas');
-  
+  var canvas = document.getElementById("canvas");
+
   canvas.height = 200;
-  canvas.width =  1900;
-  
+  canvas.width = 1900;
+
   var tempCanvas = document.createElement("canvas"),
-      tCtx = tempCanvas.getContext("2d");
+    tCtx = tempCanvas.getContext("2d");
 
   tempCanvas.width = size;
   tempCanvas.height = size;
   tCtx.drawImage(img, 0, 0, img.width, img.height, 0, 0, size, size);
 
   // use getContext to use the canvas for drawing
-  var ctx = canvas.getContext('2d');
+  var ctx = canvas.getContext("2d");
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = ctx.createPattern(tempCanvas, 'repeat');
-  
-  ctx.beginPath();
-  ctx.rect(0,0,canvas.width,canvas.height);
-  ctx.fill();
+  ctx.fillStyle = ctx.createPattern(tempCanvas, "repeat");
 
+  ctx.beginPath();
+  ctx.rect(0, 0, canvas.width, canvas.height);
+  ctx.fill();
 }
 
 var img = new Image();
 img.src = "/assets/PatternImage.png";
-img.onload = function(){
- drawPattern(this, 50);
-}
+img.onload = function () {
+  drawPattern(this, 50);
+};
